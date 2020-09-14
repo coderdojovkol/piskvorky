3e04f36c95f4fda26835e73cf4cafaa8a96d3472
vkol <coderdojovkol@gmail.com>
2020-09-14 18:23:55 +0200

    první piskvorky :)

---
+++ program.js
@@ -0,0 +1,45 @@
+var stage = new Konva.Stage({
+    container: 'container',   // id of container <div>
+    width: 500,
+    height: 500
+});
+// then create layer
+var layer = new Konva.Layer();
+
+// create our shape
+var circle = new Konva.Circle({
+    x: stage.width() / 2,
+    y: stage.height() / 2,
+    radius: 70,
+    fill: 'red',
+    stroke: 'black',
+    strokeWidth: 4
+});
+
+layer.draw();
+// add the shape to the layer
+layer.add(circle);
+
+// add the layer to the stage
+stage.add(layer);
+
+// draw the image
+layer.draw();
+var triangle = new Konva.Shape({
+    sceneFunc: function(context) {
+        context.beginPath();
+        context.moveTo(20, 50);
+        context.lineTo(220, 80);
+        context.quadraticCurveTo(150, 100, 260, 170);
+        context.closePath();
+
+        // special Konva.js method
+        context.fillStrokeShape(this);
+    },
+    fill: '#00D2FF',
+    stroke: 'black',
+    strokeWidth: 4
+});
+triangle.draggable(true)
+layer.add(triangle)
+layer.draw();
